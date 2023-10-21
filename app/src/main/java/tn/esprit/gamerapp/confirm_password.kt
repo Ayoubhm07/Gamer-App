d38a9b6bc16c52302841466d7240fbbfed9ca2d3
Ayoub <61551920+Ayoubhm07@users.noreply.github.com>
2023-10-21 15:40:46 +0100

    Working on Login & SignUp activities
    **Login activity
    **SignUp activity
    **Forgot password activity
    ** Confirm password activity
    **Verfication activity

---
+++ app/src/main/java/tn/esprit/gamerapp/confirm_password.kt
@@ -0,0 +1,11 @@
+package tn.esprit.gamerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class confirm_password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_password)
+    }
+}
